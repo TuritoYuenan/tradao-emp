c47b2aacea77cc368825d6724bfc09ad776a58ad
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-06-11 16:06:27 +1000

    feat(frontend): add title component

---
+++ webapp/src/components/Title.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { title }: { title: string } = $props();
+</script>
+
+<svelte:head>
+	<title>{title} | Tradao EMP</title>
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="{title} | Tradao EMP" />
+	<meta name="twitter:site" content="@tradaoemp" />
+	<meta name="twitter:creator" content="@turitoyuenan" />
+</svelte:head>
