9b7c24b31e4ebd828fdc5c5b190f4e736cdc8983
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-06-11 16:04:23 +1000

    refactor(frontend): extract leaf component

--- webapp/src/components/Footer.svelte
+++ webapp/src/components/Footer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Leaf from "./Leaf.svelte";
+
 	const explore = [
 		{ name: "About", href: "/" },
 		{ name: "Community", href: "/" },
@@ -7,7 +9,7 @@
 
 	const community = [
 		{ name: "GitHub", href: "https://github.com/itea-lab" },
-		{ name: "Facebook", href: "https://github.com/itea-lab" },
+		{ name: "Facebook", href: "https://facebook.com/ITeaLabTeam/" },
 		{ name: "LinkedIn", href: "https://linkedin.com/company/itea-lab" },
 	];
 </script>
@@ -52,18 +54,7 @@
 			</p>
 		</div>
 		<div id="leaf">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="230"
-				height="320"
-				viewBox="0 0 65 90"
-				style="display: block;"
-			>
-				<path
-					d="M9.76426 0C9.76426 0 17.1691 7.18412 25.1775 10.0805C84.482 31.5312 59.4707 73.3326 58.6548 73.4704C58.6548 73.4704 55.3293 67.6792 50.837 64.376C22.331 43.4199 16.5372 19.2984 16.5372 19.2984C16.5372 19.2984 22.9339 48.3243 48.2943 66.385C53.8955 70.3719 57.6882 80.1885 59.2936 90C59.2936 90 55.3232 88.3958 53.6159 87.9613C52.9501 83.6167 51.5581 79.4564 49.2048 75.74C13.7606 79.96 2.152 51.3996 0.63549 41.4408C-2.94384 17.9203 9.76426 0 9.76426 0Z"
-					style="fill: var(--foreground);"
-				/>
-			</svg>
+			<Leaf />
 		</div>
 	</section>
 </footer>
