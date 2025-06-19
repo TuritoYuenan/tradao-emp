605980f79e5969c5aa720994dfbc3f289b4d75d6
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-06-19 12:23:07 +1000

    feat(frontend): add functionality to `RegForm` component

--- frontend/src/components/RegForm.svelte
+++ frontend/src/components/RegForm.svelte
@@ -1,8 +1,55 @@
-<form>
-	<input id="ff-name" type="text" placeholder="Your Name" required />
-	<input id="ff-mail" type="email" placeholder="Your Email" required />
+<script lang="ts">
+	import supabase from "$lib/supabase";
+	import { type Database } from "$lib/models";
+	import { goto } from "$app/navigation";
 
-	<select id="ff-year" required>
+	let { eventID = "" } = $props();
+
+	let form = {
+		name: "",
+		email: "",
+		year: "" as Database["public"]["Enums"]["academic_year"] | "",
+		field: "" as Database["public"]["Enums"]["field_of_study"] | "",
+		major: "",
+		confirm: false,
+	};
+
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
+
+		if (form.year === "" || form.field === "" || form.major === "") {
+			alert("Please fill out all required fields.");
+			return;
+		}
+
+		const { data, error } = await supabase.rpc("create_event_ticket", {
+			p_event_id: eventID,
+			p_name: form.name,
+			p_email: form.email,
+			p_academic_year: form.year,
+			p_field_of_study: form.field,
+			p_major: form.major,
+			p_participate: form.confirm,
+		});
+
+		if (error) {
+			console.error("Error creating event ticket:", error);
+			return;
+		}
+
+		goto(`/tickets/${data}`);
+	}
+</script>
+
+<form>
+	<label for="ff-name">Full name</label>
+	<input id="ff-name" type="text" required bind:value={form.name} />
+
+	<label for="ff-mail">Email address</label>
+	<input id="ff-mail" type="email" required bind:value={form.email} />
+
+	<label for="ff-year">Academic Year</label>
+	<select id="ff-year" required bind:value={form.year}>
 		<option value="" disabled selected>Select Academic Year</option>
 		<option value="Freshman">Freshman</option>
 		<option value="Sophomore">Sophomore</option>
@@ -10,14 +57,16 @@
 		<option value="Senior">Senior</option>
 	</select>
 
-	<select id="ff-field" required>
+	<label for="ff-field">Field of Study</label>
+	<select id="ff-field" required bind:value={form.field}>
 		<option value="" disabled selected>Select Field of Study</option>
 		<option value="business">Business</option>
 		<option value="comp-sci">Computer Science</option>
 		<option value="mediacom">Media and Communication</option>
 	</select>
 
-	<select id="ff-major" required>
+	<label for="ff-major">Major</label>
+	<select id="ff-major" required bind:value={form.major}>
 		<option value="" disabled selected>Select Major</option>
 		<option value="DS">Data Science</option>
 		<option value="SD">Software Development</option>
@@ -27,24 +76,39 @@
 	</select>
 
 	<p>
-		<input id="ff-confm" type="checkbox" required />
+		<input
+			id="ff-confm"
+			type="checkbox"
+			required
+			bind:checked={form.confirm}
+		/>
 		<label for="ff-confm">
 			<small>By filling out this form, you agree to participate</small>
 		</label>
 	</p>
+
 	<button type="submit">Submit</button>
 </form>
 
 <style>
+	form {
+		margin-top: 0.5rem;
+	}
+
 	input[type="text"],
 	input[type="email"],
 	select {
 		width: 100%;
 		padding: 0.5rem;
-		margin-top: 1rem;
+		margin-bottom: 0.5rem;
 		background-color: transparent;
 		border: 2px solid var(--foreground);
 		border-radius: 0.5rem;
+	}
+
+	label {
+		display: inline-block;
+		margin-bottom: 0.3rem;
 	}
 
 	button {
