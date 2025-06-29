665abf8fd71e7950de7632bd6e5f0189ba56faef
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-06-29 16:56:15 +1000

    feat(frontend): add error handling page to display status and error messages

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { page } from '$app/state';
+</script>
+
+<article>
+	<h1>{page.status}</h1>
+	<p>{page.error?.message}</p>
+</article>
+
+<style>
+	article {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	h1 {
+		font-size: 2rem;
+		color: var(--foreground);
+		text-align: center;
+	}
+</style>
