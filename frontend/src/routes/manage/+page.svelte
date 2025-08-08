cd6501d8101115182d1dd743cf73794116ce6f85
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-08-08 16:00:22 +0700

    feat!(frontend): drafted management home

--- frontend/src/routes/manage/+page.svelte
+++ frontend/src/routes/manage/+page.svelte
@@ -1,18 +1,57 @@
 <script lang="ts">
 	import Banner from "$components/Banner.svelte";
 	import Title from "$components/Title.svelte";
-	import type { PageData } from "./$types";
 
-	let { data }: { data: PageData } = $props();
-
-	console.dir(data);
+	const username = "Minh-Triet";
 </script>
 
-<Title title="Ticket manager" />
+<Title title="Management Home" />
 
-<Banner title="Ticket Manager" />
+<Banner title="Welcome back, {username}" description="Manage ITea Lab events and Tradao tickets here" />
 
 <article>
-	<code>
-	</code>
+	<section>
+		<div>
+			<a href="/manage-events">
+				<span class="material-symbols-rounded">event</span>
+				<h2>Events CRUD</h2>
+			</a>
+		</div>
+		<div>
+			<a href="/manage-tickets">
+				<span class="material-symbols-rounded">local_activity</span>
+				<h2>Tickets CRUD</h2>
+			</a>
+		</div>
+		<div>
+			<a href="/manage-admins">
+				<span class="material-symbols-rounded">manage_accounts</span>
+				<h2>Admins CRUD</h2>
+			</a>
+		</div>
+	</section>
 </article>
+
+<style>
+	section {
+		max-width: 800px;
+		min-height: 30vh;
+		margin: 0 auto;
+		display: grid;
+		align-content: center;
+		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+		gap: 1rem;
+	}
+
+	section > div {
+		padding: 1rem;
+		text-align: center;
+		background-color: var(--green);
+		border-radius: 1rem;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	section span.material-symbols-rounded {
+		font-size: 6em;
+	}
+</style>
