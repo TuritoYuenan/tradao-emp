999afbd029f0014c6291cbc526f02093c8b464c6
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-09-09 22:05:12 +0700

    chore: remove sveltekit implementation (hope to see you again soon)

--- frontend/src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { type Database } from "./models";
-
-const backendURL = import.meta.env.VITE_SUPABASE_URL
-const backendKey = import.meta.env.VITE_SUPABASE_KEY
-
-const supabase = createClient<Database>(backendURL, backendKey);
-
-export default supabase;
--- frontend/src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Banner from '../../components/Banner.svelte';
-</script>
-
-<Banner
-	title="About Us"
-	description="Learn more about ITea Lab, our mission, and how we contribute to the tech community."
-/>
