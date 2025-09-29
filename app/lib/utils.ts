3a9d65129703e44994d3a1b7cbaa518664f15325
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2025-09-29 21:06:59 +0700

    feat(app): add string array support for `errorResponse()`

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,7 +4,7 @@
  * @param status Status code, defaults to 500
  * @returns HTTP Response with error message
  */
-export function errorResponse(status: number = 500, message: string) {
+export function errorResponse(status: number = 500, message: string | string[]) {
 	console.error('%d: %s', status, message);
 	return new Response(
 		JSON.stringify({ error: message }),
